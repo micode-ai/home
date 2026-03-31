2d4d57d904410fe8d1d4d15db8e2383d51d35229
Mihail Perevertkin <perevertkinma@gmail.com>
2026-03-31 16:56:45 +0300

    fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/home/',
-  plugins: [svelte({ hot: !process.env.VITEST })],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.js'],
-  },
-  resolve: process.env.VITEST
-    ? {
-        conditions: ['browser']
-      }
-    : undefined
-})
+    base: "/",
+    plugins: [svelte({ hot: !process.env.VITEST })],
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: ["./src/setupTests.js"],
+    },
+    resolve: process.env.VITEST
+        ? {
+              conditions: ["browser"],
+          }
+        : undefined,
+});
